57edb391c4291a3122ee5948b5bfa06297470dd2
pablo.paparini.ext@boehringer-ingelheim.com <pablo.paparini.ext@boehringer-ingelheim.com>
2020-11-02 23:43:05 +0100

    profiles funcionando

--- src/main/java/com/sam/gateway/controllers/Restart.java
+++ src/main/java/com/sam/gateway/controllers/Restart.java
@@ -4,14 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.UUID;
 
 @RestController
 public class Restart {
 
   @GetMapping("/restart")
-  public String restart() {
-    File f = new File("restart");
-    f.setLastModified(System.currentTimeMillis());
+  public String retrieveAllStudents() throws FileNotFoundException {
+    File file = new File("src/main/resources/restart");
+    PrintWriter writer = new PrintWriter(file.getAbsolutePath());
+    writer.print(UUID.randomUUID().toString());
+    writer.close();
+    file.setLastModified(System.currentTimeMillis());
     return "Restarting";
   }
 }
