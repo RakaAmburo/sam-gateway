d523a5651e915361c3520a3209f80d721c65ea7a
pablo.paparini.ext@boehringer-ingelheim.com <pablo.paparini.ext@boehringer-ingelheim.com>
2020-11-02 11:10:45 +0100

    profiles funcionando

--- src/main/java/com/sam/gateway/configurations/rsocket/Condenser.java
+++ src/main/java/com/sam/gateway/configurations/rsocket/Condenser.java
@@ -239,7 +239,7 @@ public class Condenser {
             }
 
             getRSocketRequester();
-            startPing();
+            //startPing();
             connect();
             connected = true;
             // startAmAlive();
