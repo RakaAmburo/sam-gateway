ca479a5effc8fc8b06cb9f7751599a08fb462c8d
pablo.paparini.ext@boehringer-ingelheim.com <pablo.paparini.ext@boehringer-ingelheim.com>
2020-11-18 21:22:23 +0100

    fixing

--- src/main/java/com/sam/gateway/controllers/Test.java
+++ src/main/java/com/sam/gateway/controllers/Test.java
@@ -1,18 +1,14 @@
 package com.sam.gateway.controllers;
 
+import com.sam.commons.entities.BigRequest;
 import com.sam.gateway.configurations.rsocket.Condenser;
-import com.sam.gateway.entities.BigRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/test")
